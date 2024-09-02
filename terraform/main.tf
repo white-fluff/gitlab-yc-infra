24fc664349f7666c38c4576c26211816030f71ae
konst <const.white@yandex.ru>
2024-09-02 17:09:33 +0300

    basi tf conf and gitlab pipeline

---
+++ terraform/main.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+
+  required_version = ">= 0.128.0"
+
+  backend "s3" {
+    endpoints = {
+      s3 = "https://storage.yandexcloud.net"
+    }
+
+    bucket = "tf-service-bucket"
+    region = "ru-central1"
+    key    = "infra.tfstate"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+
+    dynamodb_endpoint = "https://docapi.serverless.yandexcloud.net/ru-central1/b1g02u7oe05n4bt41qpi/etnh0gd932lqlgnqci28"
+    dynamodb_table = "tf_lock"
+  }
+}
+
+provider "yandex" {
+  token     = var.iam_token
+  cloud_id  = var.cloud_id
+  folder_id = var.folder_id
+  zone      = var.zone
+}
+
+# Just Test Pipeline
+
+resource "yandex_iam_service_account" "sa-test" {
+  name        = "test-sa"
+  description = "Testing creation of a resource by terraform pipeline via gitlab"
+  folder_id   = var.folder_id
+}
+
+# module "compute-instance" {
+#   source         = "./modules/compute-instance"
+#   default_zone   = var.zone
+#   folder_id      = var.folder_id
+# }
