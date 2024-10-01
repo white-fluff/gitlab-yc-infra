183aadf2f802420fb652eb3673b7cb62b086283f
konst <const.white@yandex.ru>
2024-10-01 16:44:33 +0300

    adding bot

---
+++ infra-bot/main.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+	"time"
+
+	tele "gopkg.in/telebot.v3"
+)
+
+var clusterID string = os.Getenv("K8S_CLUSTER_ID")
+var allowedUsers []int64
+
+func init() {
+	envAllowedUser := os.Getenv("ALLOWED_USER_ID")
+
+	allowedUser, err := strconv.ParseInt(envAllowedUser, 10, 64)
+	if err != nil {
+		log.Println("Something went wrong during converting data types:", err)
+		return
+	}
+
+	allowedUsers = []int64{
+		allowedUser,
+	}
+}
+
+func isAllowedUser(userID int64) bool {
+	for _, id := range allowedUsers {
+		if id == userID {
+			return true
+		}
+	}
+	return false
+}
+
+func start_k8s_cluster() string {
+	var reqURL string = "https://mks.api.cloud.yandex.net/managed-kubernetes/v1/clusters/" + (clusterID) + ":start"
+	resp, err := http.NewRequest("POST", reqURL, nil)
+	log.Println("Start K8s Cluster. ID: " + (reqURL) + "\n")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return string(body)
+}
+
+func stop_k8s_cluster() string {
+	var reqURL string = "https://mks.api.cloud.yandex.net/managed-kubernetes/v1/clusters/" + (clusterID) + ":start"
+	resp, err := http.NewRequest("POST", reqURL, nil)
+	log.Println("Stop K8s Cluster. ID: " + (reqURL) + "\n")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return string(body)
+}
+
+func main() {
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		<-quit
+		log.Println("The bot is stopped")
+		os.Exit(0)
+	}()
+
+	pref := tele.Settings{
+		Token:  os.Getenv("TOKEN"),
+		Poller: &tele.LongPoller{Timeout: 10 * time.Second},
+	}
+
+	bot, err := tele.NewBot(pref)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	bot.Handle("/start", func(c tele.Context) error {
+		user := c.Sender()
+		userID := user.ID
+
+		if !isAllowedUser(userID) {
+			return c.Send("Sorry, you don't have access to this bot.")
+		}
+
+		username := user.Username
+		message := "Hello, " + string(username) + "! Nice to meet you!\n" + "Your ID is " + strconv.FormatInt(userID, 10) + "\n"
+
+		return c.Send(message)
+	})
+
+	bot.Handle("/start_k8s_cluster", func(c tele.Context) error {
+
+		result := start_k8s_cluster()
+		message := "```\n" + (result) + "\n```"
+
+		return c.Send(message)
+	})
+
+	bot.Handle("/stop_k8s_cluster", func(c tele.Context) error {
+
+		result := stop_k8s_cluster()
+		message := "```\n" + (result) + "\n```"
+
+		return c.Send(message)
+	})
+
+	log.Println("The bot is running")
+	bot.Start()
+}
