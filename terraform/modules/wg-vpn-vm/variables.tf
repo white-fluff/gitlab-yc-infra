d290506a90bd5c4ba87bd37194853cfe21403bf6
konst <const.white@yandex.ru>
2024-11-21 11:28:55 +0300

    start tf pipe

--- terraform/modules/wg-vpn-vm/variables.tf
+++ terraform/modules/wg-vpn-vm/variables.tf
@@ -26,7 +26,7 @@ variable "boot_disk_type" {
 variable "boot_disk_size" {
   description = "Boot disk size"
   type        = number
-  default     = 10737418240
+  default     = 10
 }
 
 variable "boot_disk_image_id" {
